f0d25c3ef8661d6fb826c4bf110c7471324d001b
jvaleriom <jvaleriom@vallegrande.edu.pe>
2019-10-26 02:59:19 -0500

    Creacion de un Modulo Colegio en Odoo

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Colegio Juan Valerious',
+    'summary': """Nuevo Modelo de Colegio solo de para presentar de Prueba""",
+    'version': '12',
+    'description': """Nuevo Modelo de Colegio solo de para presentar de Prueba""",
+    'author': 'Juan Valerio',
+    'company': 'Sepulveda',
+    'website': 'http://www.valerious.com',
+    'category': 'Test',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'views/view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
